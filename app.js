6a2adaba346bcf45e650cc83999fa123b4fea69b
yousef <yousefahmedelshahed@gmail.com>
2023-10-22 21:45:56 +0300

    change socket backend url

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["https://menuonlinedashboard.vercel.app", "https://menuonlineclient.vercel.app"],
   },
 });
 
